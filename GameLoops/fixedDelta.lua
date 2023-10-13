deaee1250a0f6922132dc9066f0f65bb9bfbcffc
Thomas De Maen <mr-sylar59@world-of-sylar.fr>
2023-10-13 19:11:13 +0200

    fix indent

--- GameLoops/fixedDelta.lua
+++ GameLoops/fixedDelta.lua
@@ -1,38 +1,38 @@
 function love.run()
     if love.math then love.math.setRandomSeed(os.time()) end
 
-	if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+    if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
 
     -- Fixed 60FPS dt
-	local dt = 1 / 60
+    local dt = 1 / 60
 
-	-- Main loop time.
-	return function()
-		-- Process events.
-		if love.event then
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
-				if name == "quit" then
-					if not love.quit or not love.quit() then
-						return a or 0
-					end
-				end
-				love.handlers[name](a,b,c,d,e,f)
-			end
-		end
+    -- Main loop time.
+    return function()
+        -- Process events.
+        if love.event then
+            love.event.pump()
+            for name, a,b,c,d,e,f in love.event.poll() do
+                if name == "quit" then
+                    if not love.quit or not love.quit() then
+                        return a or 0
+                    end
+                end
+                love.handlers[name](a,b,c,d,e,f)
+            end
+        end
 
-		-- Call update and draw
-		if love.update then love.update(dt) end
+        -- Call update and draw
+        if love.update then love.update(dt) end
 
-		if love.graphics and love.graphics.isActive() then
-			love.graphics.origin()
-			love.graphics.clear(love.graphics.getBackgroundColor())
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            love.graphics.clear(love.graphics.getBackgroundColor())
 
-			if love.draw then love.draw() end
+            if love.draw then love.draw() end
 
-			love.graphics.present()
-		end
+            love.graphics.present()
+        end
 
-		if love.timer then love.timer.sleep(0.001) end
-	end
+        if love.timer then love.timer.sleep(0.001) end
+    end
 end
--- GameLoops/freePhysic.lua
+++ GameLoops/freePhysic.lua
@@ -1,33 +1,33 @@
 function love.run()
     if love.math then love.math.setRandomSeed(os.time()) end
 
-	if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
+    if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
 
-	-- We don't want the first frame's dt to include time taken by love.load.
-	if love.timer then love.timer.step() end
+    -- We don't want the first frame's dt to include time taken by love.load.
+    if love.timer then love.timer.step() end
 
     -- Rendu physique à 100FPS
-	local dt = 1 / 100
+    local dt = 1 / 100
     local accum = 0
 
-	-- Main loop time.
-	return function()
-		-- Process events.
-		if love.event then
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
-				if name == "quit" then
-					if not love.quit or not love.quit() then
-						return a or 0
-					end
-				end
-				love.handlers[name](a,b,c,d,e,f)
-			end
-		end
+    -- Main loop time.
+    return function()
+        -- Process events.
+        if love.event then
+            love.event.pump()
+            for name, a,b,c,d,e,f in love.event.poll() do
+                if name == "quit" then
+                    if not love.quit or not love.quit() then
+                        return a or 0
+                    end
+                end
+                love.handlers[name](a,b,c,d,e,f)
+            end
+        end
 
         local frameTime
-		-- Update dt, as we'll be passing it to update
-		if love.timer then frameTime = love.timer.step() end
+        -- Update dt, as we'll be passing it to update
+        if love.timer then frameTime = love.timer.step() end
         accum = accum + frameTime
 
         while accum > dt do
@@ -37,15 +37,15 @@ function love.run()
         end
 
         -- Call draw
-		if love.graphics and love.graphics.isActive() then
-			love.graphics.origin()
-			love.graphics.clear(love.graphics.getBackgroundColor())
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            love.graphics.clear(love.graphics.getBackgroundColor())
 
-			if love.draw then love.draw() end
+            if love.draw then love.draw() end
 
-			love.graphics.present()
-		end
+            love.graphics.present()
+        end
 
-		if love.timer then love.timer.sleep(0.001) end
-	end
+        if love.timer then love.timer.sleep(0.001) end
+    end
 end
